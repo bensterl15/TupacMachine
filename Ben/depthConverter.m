08015ff3a21034880bdf1e4ae0a69ed91bef8c7e
root <bensterl15@gmail.com>
2019-03-25 21:35:10 +0000

    Finished the python conversion

--- Ben/depthConverter.m
+++ Ben/depthConverter.m
@@ -19,11 +19,7 @@ eye_displacement = floor(p_density*f*tan(theta));
 depth = imread('Cubic_Depth.jpg');
 depth = imresize(depth,0.1);
 #Quantize the depth information:
-#depth = 5*floor(depth/5);
 depth = double(depth)/(255*4) + 0.25;
-
-#imshow(depth);
-#figure;
 
 l_depth_scaling = depth.*double(repmat([flip(0:(height - eye_displacement - 1)) 0:(eye_displacement - 1)],width,1));
 r_depth_scaling = depth.*double(repmat([flip(0:(eye_displacement - 1)) 0:(height - eye_displacement - 1)],width,1));
@@ -32,21 +28,15 @@ r_depth_scaling = depth.*double(repmat([flip(0:(eye_displacement - 1)) 0:(height
 l_depth_scaling = l_depth_scaling / max(max(l_depth_scaling)) + 1;
 r_depth_scaling = r_depth_scaling / max(max(r_depth_scaling)) + 1;
 
-#{
-subplot(121);
-imshow(l_depth_scaling - 1);
-subplot(122);
-imshow(r_depth_scaling - 1);
-#}
+left = zeros(width,2.5*height);
+right = zeros(width,2.5*height);
 
-left = zeros(width,2*height);
-right = zeros(width,2*height);
+size(left)
 
 orientation = 0;
 
 for i = 1:width
 	for j = 1:height
-
 		#Left Image Loop:
 		val = screen(i,j);
 		depth = l_depth_scaling(i,j);
@@ -74,25 +64,31 @@ for i = 1:width
 	endfor
 endfor
 
+abs(floor(new_height)) + 1
+
+threshold = 40;
 for k = 1:2
 for i = 1:width
-	for j = 2:(2*height - 1)
-		if(right(i,j) == 0)
+	for j = 2:(2.5*height - 1)
+		if(right(i,j) < threshold)
 			right(i,j) = (right(i,j - 1) + right(i,j + 1))/2;
 		endif
 
-		if(left(i,j) == 0)
+		if(left(i,j) < threshold)
 			left(i,j) = (left(i,j - 1) + left(i,j + 1))/2;
 		endif
 	endfor
 endfor
 endfor
 
+#size(left)
+#height
+#{
 subplot(121);
 imshow(mat2gray(left));
 subplot(122);
 imshow(mat2gray(right));
-
+#}
 imwrite(mat2gray(right),'right.jpg');
 imwrite(mat2gray(left),'left.jpg');
 
--- Ben/depthConverter.py
+++ Ben/depthConverter.py
@@ -3,6 +3,7 @@ import matplotlib.image as mpimg
 import matplotlib.pyplot as plt
 from skimage import color
 from skimage.transform import downscale_local_mean
+import math
 
 image_name = 'Cubic_Structure.jpg'
 depth_name = 'Cubic_Depth.jpg'
@@ -24,20 +25,61 @@ eye_displacement = np.floor(p_density*f*np.tan(theta))
 
 depth = color.rgb2gray(mpimg.imread(depth_name))
 depth = downscale_local_mean(depth,(10,10))
-depth = depth/(255*4) + 0.25
+depth = depth/4+0.25;
 
 l_depth_scaling = np.matlib.repmat(list(range(int(height - eye_displacement)))[::-1] + list(range(int(eye_displacement))),width,1)
 r_depth_scaling = np.matlib.repmat(list(range(int(eye_displacement)))[::-1] + list(range(int(height - eye_displacement))),width,1)
 
-#l_depth_scaling = np.multiply(l_depth_scaling,np.array(depth))
-for i in range(l_depth_scaling.shape[0]):
-	for j in range(l_depth_scaling.shape[1]):
-		l_depth_scaling[i,j] = l_depth_scaling[i,j]*depth[i,j]
+l_depth_scaling = np.multiply(l_depth_scaling,np.array(depth))
 
 l_depth_scaling = l_depth_scaling / l_depth_scaling.max() + 1
 r_depth_scaling = r_depth_scaling / r_depth_scaling.max() + 1
 
+#plt.imshow(l_depth_scaling,cmap='Greys',interpolation='nearest')
+#plt.show()
 
+left = np.zeros((width, int(2.5*height)))
+right = np.zeros((width, int(2.5*height)))
+orientation = 0
 
-plt.imshow(l_depth_scaling,cmap='Greys',interpolation='nearest')
-plt.show()
+for i in range(1,width+1):
+	for j in range(1,height+1):
+		index_j = j-1
+		index_i = i-1		
+		# Left Image Loop
+		val = screen[index_i][index_j]
+		depth = l_depth_scaling[index_i][index_j]
+
+		if j < height - eye_displacement - 1:
+			orientation = -1
+		else:
+			orientation = 1
+
+		new_height = j + height + orientation * depth * abs(height - eye_displacement - j)
+		left[index_i][abs(math.floor(new_height)) - 1] = val
+
+		# Right Image Loop
+		depth = r_depth_scaling[index_i][index_j]
+		if j < eye_displacement + 1:
+			orientation = -1
+		else:
+			orientation = 1
+
+		new_height = j + orientation * depth * abs(j-eye_displacement)
+		right[index_i][abs(math.floor(new_height))] = val
+
+threshold = 30/255
+for k in range(2):
+	for i in range(width):
+		for j in range(2*height-1):
+			if right[i][j] < threshold:
+				right[i][j] = right[i][j-1] + right[i][j+1]/2
+			if left[i][j] < threshold:
+				left[i][j] = left[i][j-1] + left[i][j+1]/2
+
+fig = plt.figure()
+plt.subplot(121)
+plt.imshow(left,cmap='Greys',interpolation='nearest')
+plt.subplot(122)
+plt.imshow(right,cmap='Greys',interpolation='nearest')
+fig.savefig('plot.png')
